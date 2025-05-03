4c4edfe1b5176681f19b882cd68fa834fa82e652
Caicai Chen <chen@Caicais-MacBook-Pro.local>
2025-05-03 01:54:22 -0400

    Add run.sh to compile libraries before run test and benchmark

---
+++ run.sh
@@ -0,0 +1,8 @@
+g++ -std=c++17 -O3 -c tdm/NTT.cpp -o tdm/NTT.o -I/opt/homebrew/include
+ar rcs TDM/libNTT.a TDM/NTT.o
+
+g++ -std=c++17 -O3 -c mvp/mvp.cpp -o mvp/mvp.o -I/opt/homebrew/include
+ar rcs mvp/libMVP.a mvp/mvp.o
+
+g++ -std=c++17 -O3 -c ecc/ReedSolomon.cpp -o ecc/ReedSolomon.o -I/opt/homebrew/include
+ar rcs ecc/libReedSolomon.a ecc/ReedSolomon.o
